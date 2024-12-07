724180e1ca44743dab926cc66637609d9e9d7d69
Niraj Punde <prophet.niraj@gmail.com>
2024-12-07 16:15:03 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_posRozHAw', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '229f04hua6rson8ips7kr4il3f', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '2001vrnfsosq4l6lvumj9b2l8m', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
